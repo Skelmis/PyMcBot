f9307393807035ff690d29be3d6547c6e2fe207d
Skelmis <Skelmis.craft@gmail.com>
2020-02-11 05:55:33 +1300

    Added support for sending ingame chat within classes

--- cogs/ingame.py
+++ cogs/ingame.py
@@ -21,42 +21,19 @@ import functools
 
 import cogs._json
 
-global server_chat
-server_chat = ""
-
-global ingame_commands
-ingame_commands = "Placeholder"
-
-connected = False
-
-async def SendChat(self):
-    print("Running send chat")
-    channel = self.bot.get_channel(int(self.bot.channel))
-    while True:
-        global server_chat
-        if not server_chat:
-            pass
-        else:
-            if server_chat != 'Placeholder':
-                await channel.send(f"```{server_chat}```")
-            server_chat = ""
-        await asyncio.sleep(0.5)
-
 class Alts():
 
-    def __init__(self, username, password):
+    def __init__(self, bot, username, password):
         self.username = username
         self.password = password
         self.admins = ['Skelmis']
+        self.loop = asyncio.get_event_loop()
+        self.ingame = Ingame(bot)
 
     def send_chat(self, message):
         packet = serverbound.play.ChatPacket()
         packet.message = message
         self.connection.write_packet(packet)
-
-    def test():
-        def testtwo():
-            print("returns")
 
     def connect(self, server):
 
@@ -84,7 +61,7 @@ class Alts():
             handle_join_game, clientbound.play.JoinGamePacket)
 
         def print_chat(chat_packet):
-            global server_chat
+            #global server_chat
             data = "{}" .format(chat_packet.json_data)
             data = data.replace("true", "True")
             data = data.replace("false", "False")
@@ -106,7 +83,6 @@ class Alts():
                 pass
             string = ' '.join(list)
             string = re.sub('\§c|\§f|\§b|\§d|\§a|\§1|\§2|\§3|\§4|\§5|\§6|\§7|\§8|\§9|\§0', '', string)
-            server_chat = server_chat + string + '\n'
 
             try:
                 found = re.search('(.+?)  has requested that you teleport to them.', string).group(1)
@@ -114,6 +90,8 @@ class Alts():
                     self.send_chat('/tpyes')
             except AttributeError:
                 pass
+
+            self.loop.create_task(self.ingame.SendChatToDiscord(string))
 
         self.connection.register_packet_listener(
             print_chat, clientbound.play.ChatMessagePacket)
@@ -155,10 +133,10 @@ class Ingame(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.chat_check_loop.start()
 
-    def cog_unload(self):
-        self.check_task.cancel()
+    async def SendChatToDiscord(self, message):
+        channel = self.bot.get_channel(int(self.bot.channel))
+        await channel.send(f"```{message}```")
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -169,16 +147,9 @@ class Ingame(commands.Cog):
         print("Attempting Mc connection.")
         await asyncio.sleep(10)
         loop = asyncio.get_event_loop()
-        self.bot.account = Alts(self.bot.username, self.bot.password)
+        self.bot.account = Alts(self.bot, self.bot.username, self.bot.password)
         thing = functools.partial(self.bot.account.connect, self.bot.server)
         blockReturn = await loop.run_in_executor(ThreadPoolExecutor(), thing)
-
-    @commands.command()
-    async def checktest(self, ctx):
-        #self.bot.account
-        #thing = functools.partial(self.bot.account.test.testtwo, self.bot.server)
-        loop = asyncio.get_event_loop()
-        blockReturn = await loop.run_in_executor(ThreadPoolExecutor(), self.bot.account.test.testtwo)
 
     @commands.command()
     @commands.is_owner()
@@ -198,7 +169,7 @@ class Ingame(commands.Cog):
         for account in alts:
             username, password = account.split(':')
             await message.edit(content=f'Testing: {username}')
-            await CheckAlt(username, password)
+            await CheckAlt(self.bot, username, password)
             await asyncio.sleep(2.5)
         await message.edit(content='Testing complete')
 
@@ -212,58 +183,24 @@ class Ingame(commands.Cog):
 
     @commands.command()
     @commands.is_owner()
+    @commands.cooldown(1, 1.5, commands.BucketType.default)
     async def send(self, ctx, *, message):
         loop = asyncio.get_event_loop()
         thing = functools.partial(self.bot.account.send_chat, message)
         blockReturn = await loop.run_in_executor(ThreadPoolExecutor(), thing)
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        await asyncio.sleep(15)
-        print("Attempting to run send chat")
-        await SendChat(self)
-
     @commands.command()
     @commands.is_owner()
     @commands.cooldown(1, 1.5, commands.BucketType.default)
-    async def sudo(self, ctx, *, command):
-        global ingame_commands
-        if ingame_commands == "Placeholder":
-            ingame_commands = command + '\n'
-        else:
-            ingame_commands = ingame_commands + commands + '\n'
-        await ctx.send(f"Sudoing: `{command}`")
+    async def reply(self, ctx, *, message):
+        message = f'/r {message}'
+        loop = asyncio.get_event_loop()
+        thing = functools.partial(self.bot.account.send_chat, message)
+        blockReturn = await loop.run_in_executor(ThreadPoolExecutor(), thing)
 
-    @commands.command()
-    @commands.is_owner()
-    @commands.cooldown(1, 1.5, commands.BucketType.default)
-    async def reply(self, ctx, *, command):
-        global ingame_commands
-        if ingame_commands == "Placeholder":
-            ingame_commands = '/r ' + command + '\n'
-        else:
-            ingame_commands = ingame_commands + '/r ' + commands + '\n'
-        await ctx.send(f"Replying: `{command}`")
-
-    @loop(seconds=1)
-    async def chat_check_loop(self):
-        if self.bot.ingame_text != "Placeholder":
-            global ingame_commands
-            commandList = self.bot.ingame_text.split("\n")
-            for command in commandList:
-                if ingame_commands == "Placeholder":
-                    ingame_commands = command + '\n'
-                else:
-                    ingame_commands = ingame_commands + command + '\n'
-            self.bot.ingame_text = "Placeholder"
-
-    @chat_check_loop.before_loop
-    async def before_chat_check_loop(self):
-        await self.bot.wait_until_ready()
-
-async def CheckAlt(username, password):
+async def CheckAlt(bot, username, password):
     loop = asyncio.get_event_loop()
-    account = Alts(username, password)
+    account = Alts(bot, username, password)
     thing = functools.partial(account.verify, 'mc-central.net')
     blockReturn = await loop.run_in_executor(ThreadPoolExecutor(), thing)
     return

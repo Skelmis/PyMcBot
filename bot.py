8a4eabdbc0a0c868b826413c8477beb18631bd64
skelmis <skelmis.craft@gmail.com>
2020-10-19 18:28:25 +1300

    :sparkles: Add further help and error handling

--- bot.py
+++ bot.py
@@ -6,20 +6,20 @@ import sys, traceback
 from pathlib import Path
 import os
 
-import cogs._json
+from utils.jsonLoader import read_json
 
 cwd = Path(__file__).parents[0]
 cwd = str(cwd)
 
-secret_file = cogs._json.read_json("token")
-config_file = cogs._json.read_json("config")
+secret_file = read_json("token")
+config_file = read_json("config")
 
 
 def get_prefix(bot, message):
     return commands.when_mentioned_or(bot.PREFIX)(bot, message)
 
 
-bot = commands.Bot(command_prefix=get_prefix, case_insensitive=True)
+bot = commands.Bot(command_prefix=get_prefix, case_insensitive=True, help_command=None)
 bot.config_token = secret_file["token"]
 
 bot.PREFIX = "--"
--- utils/util.py
+++ utils/util.py
@@ -2,6 +2,15 @@ import asyncio
 import random
 
 import discord
+from discord.ext.buttons import Paginator
+
+
+class Pag(Paginator):
+    async def teardown(self):
+        try:
+            await self.page.clear_reactions()
+        except discord.HTTPException:
+            pass
 
 
 async def GetMessage(
